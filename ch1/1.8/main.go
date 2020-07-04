b0c39a34c0768cd4fd2ab82f7a6d92b8312b96a4
8lall0 <savino.liguori@gmail.com>
2020-07-04 16:14:22 +0200

    :zap: 1.8

---
+++ ch1/1.8/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "https://") {
+			url = "https://" + url
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+
+		_, _ = io.Copy(os.Stdout, resp.Body)
+		_ = resp.Body.Close()
+	}
+}

75eb5d7de3f695c77b6c8a84b4646fd83365af22
Savino Pio Liguori <8lall0@users.noreply.github.com>
2021-11-15 19:52:00 +0100

    :zap: ch1|1.9

---
+++ ch1/exercises/1.9/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		newUrl := url
+		if !strings.HasPrefix(url, "http://") {
+			newUrl = "http://" + url
+		}
+		resp, err := http.Get(newUrl)
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		_, err = io.Copy(os.Stdout, resp.Body)
+		fmt.Printf("Code: %d", resp.StatusCode)
+		_ = resp.Body.Close()
+
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", newUrl, err)
+			os.Exit(1)
+		}
+
+	}
+}
